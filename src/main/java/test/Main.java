f547897104c4f379d69f20b22e0bff1f644e9256
xcv58 <chenyihonglove@gmail.com>
2015-08-27 16:30:25 -0400

    add TreeNode.java and codes in Main.java

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -1,7 +1,21 @@
 package test;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        // HashMap<String, String> map1 = new HashMap<String, String>();
+        // HashMap<String, String> map2 = new HashMap();
+        // map2 = map1;
+        // int a = maybe ("abc") { 1, 2, 3};
+
+        TreeNode root = new TreeNode();
+        System.out.println(root.val);
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("i: " + args[i]);
+        }
+
         System.out.println("Hello World!");
     }
 }
---
+++ src/main/java/test/TreeNode.java
@@ -0,0 +1,15 @@
+package test;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode() {
+        this(0);
+    }
+    
+    public TreeNode(int v) {
+        this.val = v;
+    }
+}
